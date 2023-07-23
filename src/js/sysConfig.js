21a930d850fe5942daacd2bce2ed7b05845a6166
limingoo <liming_tennis@163.com>
2023-07-23 14:11:09 +0800

    dev

--- src/js/sysConfig.js
+++
@@ -1,9 +0,0 @@
-const  systemConfiguration={
-    github:{
-        "projectUrl":"https://github.com/apache/incubator-streampark",
-        "projectReleaseUrl":"https://github.com/apache/incubator-streampark/releases",
-        "projectIssueUrl":"https://github.com/apache/incubator-streampark/issues",
-        "projectPrUrl":"https://github.com/apache/incubator-streampark/pulls",
-    },
-}
-export  default systemConfiguration
